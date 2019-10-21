b3704fad997ba0da3d9ae7f9ac8e651ce5532dd5
@ontheroad_jp <dev@ontheroad.jp>
2019-10-21 21:48:21 +0900

    initial commit

---
+++ 3.3.0/docker-entrypoint-ex.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+user="redmine"
+group="redmine"
+
+# -----------------------------
+# UID & GID
+# -----------------------------
+if [ -n "${TOYBOX_GID}" ] && ! cat /etc/group | awk 'BEGIN{ FS= ":" }{ print $3 }' | grep ${TOYBOX_GID} > /dev/null 2>&1; then
+    groupmod -g ${TOYBOX_GID} ${group}
+    echo "GID of ${group} has been changed."
+fi
+
+if [ -n "${TOYBOX_UID}" ] && ! cat /etc/passwd | awk 'BEGIN{ FS= ":" }{ print $3 }' | grep ${TOYBOX_UID} > /dev/null 2>&1; then
+    usermod -u ${TOYBOX_UID} ${user}
+    echo "UID of ${user} has been changed."
+fi
+
+exec /docker-entrypoint.sh $@
